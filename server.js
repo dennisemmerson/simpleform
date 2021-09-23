3742725fad3a74752d8e5b0710bf3cda7a0ab5f8
dennisemmerson <dennismichaelemmerson@gmail.com>
2021-09-24 08:53:16 +0900

    Changed port access location

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(dbConfig.url)
 require('./app/routes/user.route.js')(app);
  
 // Create a Server
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 5000 , function () {
  
   var host = server.address().address
   var port = server.address().port
